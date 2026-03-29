14e454adfc951d3a0249a0c121f2696ee6b6256e
Taskeren <r0yalist@outlook.com>
2026-03-29 17:59:33 +0800

    Resolve

--- src/translatools/paratranz.py
+++ src/translatools/paratranz.py
@@ -10,19 +10,20 @@ class Paratranz:
     token: str
     _client: Optional[httpx.AsyncClient]
 
-    def __init__(self, token: str, client: Optional[httpx.AsyncClient] = None):
+    def __init__(self, token: str):
         self.token = token
-        self._client = client
-        if len(token) == 0:
+        if token is None or len(token) == 0:
             raise ValueError("A Paratranz token must be provided")
 
     async def __aenter__(self):
         if self._client is None:
             self._client = httpx.AsyncClient(headers={"Authorization": f"Bearer {self.token}"})
+        return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         if self._client is not None:
             await self._client.aclose()
+            self._client = None  # allow reuse of the Paratranz instance
 
     async def get_file_list(self, paratranz_project_id: int) -> dict[str, Any]:
         url = f"{BASE_URL}/projects/{paratranz_project_id}/files"
@@ -36,14 +37,16 @@ class Paratranz:
 
     async def put_file(self, paratranz_project_id: int, path: Path, relative_path: Path):
         url = f"{BASE_URL}/projects/{paratranz_project_id}/files"
-        resp = await self._client.post(url, files={"file": open(path, mode="rb")},
-                                       data={"path": relative_path.parent.as_posix(), "filename": relative_path.name})
-        resp.raise_for_status()
+        with open(path, mode="rb") as f:
+            resp = await self._client.post(
+                url, files={"file": f}, data={"path": relative_path.parent.as_posix(), "filename": relative_path.name})
+            resp.raise_for_status()
 
     async def update_file(self, paratranz_project_id: int, file_id: str, path: Path):
         url = f"{BASE_URL}/projects/{paratranz_project_id}/files/{file_id}"
-        resp = await self._client.post(url, files={"file": open(path, mode="rb")})
-        resp.raise_for_status()
+        with open(path, mode="rb") as f:
+            resp = await self._client.post(url, files={"file": f})
+            resp.raise_for_status()
 
     async def get_translated_file(self, paratranz_project_id: int, file_id: int) -> str:
         url = f"{BASE_URL}/projects/{paratranz_project_id}/files/{file_id}/translation"
